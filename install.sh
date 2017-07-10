b0fde2d785631a8e25e3154653872ad1a2c14674
cMDev <preet.makz@gmail.com>
2017-07-10 21:44:34 +0530

    Renamed install.sh and fixed main executable

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-rm -rf /tmp1
-git clone https://github.com/ABHIRAMSHIBU/fix_access -b v1.0 /tmp1/
+#!/bin/bash
+git clone https://github.com/ABHIRAMSHIBU/fix_access -b v1.0 /tmp/fix_access
 file="/usr/bin/fix_access"
 if [ -f "$file" ]
 then
     echo "Hey $USER, Do you really want to copy this file? this action will overright the existing file!"
 else
-	cp -rv /tmp1/fix_access /usr/bin/
+	cp -rv /tmp/fix_access/fix_access /usr/bin/
 fi
 rm -rf /tmp1
